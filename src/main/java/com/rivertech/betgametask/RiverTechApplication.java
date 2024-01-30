3a525cfc272c6fbbccb91688d09aa43e8ec1c0fc
Enrique Barca <barcaenrique7@gmail.com>
2024-01-30 19:11:10 +0000

    close gh-1 Base app skeleton

---
+++ src/main/java/com/rivertech/betgametask/RiverTechApplication.java
@@ -0,0 +1,13 @@
+package com.rivertech.betgametask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiverTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiverTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rivertech/betgametask/RiverTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rivertech.betgametask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiverTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
