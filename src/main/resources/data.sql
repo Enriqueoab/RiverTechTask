642a3b9ad94bb43935e9f4cade9375583bf2cc47
Enrique Barca <barcaenrique7@gmail.com>
2024-02-02 23:38:59 +0000

    save resources files

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 
 MERGE INTO game (id, description)
     VALUES (1, 'Football match'),
-           (2, 'Tennis match'),
-           (3, 'Quidditch match');
+    (2, 'Tennis match'),
+    (3, 'Quidditch match');
